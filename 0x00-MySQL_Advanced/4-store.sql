95d559fba1ab0f4f7f538b1d93f0df58a34cc67a
psych0wh1z <ikeagudike@gmail.com>
2023-08-10 17:57:01 +0100

    Add task4 file

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -3,12 +3,12 @@
 DELIMITER //
 
 CREATE TRIGGER update_quantity
- AFTER INSERT  ON orders
-   FOR EACH ROW
+ AFTER  INSERT ON orders
+   FOR    EACH ROW
  BEGIN
-       UPDATE items
-          SET quantity = quantity - NEW.number
-        WHERE name = NEW.item_name;
+        UPDATE items
+           SET quantity = quantity - NEW.number
+         WHERE name = NEW.item_name;
  END//
 
 DELIMTER ;
