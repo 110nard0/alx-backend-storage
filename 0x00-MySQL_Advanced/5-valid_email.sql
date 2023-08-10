1a31ba797418e85d93011ef9c82edccea4a4488a
psych0wh1z <ikeagudike@gmail.com>
2023-08-10 20:21:35 +0100

    Rework statement

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,18 +1,16 @@
 -- creates a trigger that resets the valid_email attribute when email changes
 
+-- Create the trigger
 DELIMITER //
 
-CREATE TRIGGER reset_valid_email
+CREATE TRIGGER update_valid_email
 BEFORE  UPDATE ON users
    FOR    EACH ROW
  BEGIN
             IF NEW.email <> OLD.email THEN
-                IF OLD.valid_email = 0 THEN
-                      SET NEW.valid_email = 1;
-            ELSEIF OLD.valid_email = 1 THEN
-                      SET NEW.valid_email = 0;
-               END IF;
+               SET NEW.valid_email = 0;
            END IF;
- END//
+  END;
+//
 
-DELIMTER ;
+DELIMITER ;
