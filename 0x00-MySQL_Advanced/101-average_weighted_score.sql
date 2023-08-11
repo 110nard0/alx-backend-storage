02a870eea2f1e620737fa4d01a988f9713342aa0
psych0wh1z <ikeagudike@gmail.com>
2023-08-11 06:40:18 +0100

    Add task13 files

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,44 +1,40 @@
 -- creates a stored procedure ComputeAverageWeightedScoreForUsers
 -- that computes and store the average weighted score for all students
--- that computes and store the average weighted score for a student
 
-DELIMITER $$
+   DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+DELIMITER ||
  CREATE PROCEDURE
-        ComputeAverageWeightedScoreForUsers
+        ComputeAverageWeightedScoreForUsers()
   BEGIN
+        DECLARE done INT DEFAULT FALSE;
         DECLARE user_id INT;
-        DECLARE total_score FLOAT;
-        DECLARE total_weight INT;
-        DECLARE user_avg FLOAT;
-
         DECLARE cur CURSOR
             FOR SELECT id FROM users;
+        DECLARE CONTINUE HANDLER
+            FOR NOT FOUND
+            SET done = TRUE;
 
            OPEN cur;
 
-        read_loop: LOOP
-          FETCH cur INTO user_id;
-             IF user_id IS NULL THEN
-          LEAVE read_loop;
-            END IF;
+           -- start loop and fetch id for each user
+     read_loop: LOOP
+                FETCH cur INTO user_id;
+                   IF done THEN
+                      LEAVE read_loop;
+                  END IF;
 
-                -- Calculate the average weighted score for the user
-                SELECT SUM(c.score * p.weight), SUM(p.weight)
-                  INTO total_score, total_weight
-                  FROM corrections AS c
-                  JOIN projects AS p
-                    ON c.project_id = p.id
-                 WHERE c.user_id = user_id;
+                -- Calculate and update user's average weighted score
+                UPDATE users AS u
+                   SET average_score = (
+                       SELECT SUM(c.score * p.weight) / SUM(p.weight)
+                         FROM corrections AS c
+                         JOIN projects AS p
+                           ON c.project_id = p.id
+                        WHERE c.user_id = user_id)
+                 WHERE u.id = user_id;
 
-                   SET user_avg = total_score / total_weight;
-
-                -- Update user's average score
-                UPDATE users
-                   SET average_score = user_avg
-                 WHERE id = user_id;
-
+            -- end loop and close cursor
             END LOOP;
           CLOSE cur;
-   END;
-$$
+   END||
 DELIMITER ;
