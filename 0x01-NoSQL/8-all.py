a84f6ae8466b1139d64c902be9a44a704ff78fab
psych0wh1z <ikeagudike@gmail.com>
2023-08-15 08:29:26 +0100

    Add task8 files

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""Module 8-all"""
+
+
+def list_all(mongo_collection):
+    """Lists all documents in a MongoDB collection
+
+    Argument:
+        mongo_collection (Collection): pymongo collection object
+
+    Return:
+        documents (list): documents in valid collection or [] if None
+    """
+    documents = []
+
+    for document in mongo_collection.find():
+        documents.append(document)
+
+    return documents
