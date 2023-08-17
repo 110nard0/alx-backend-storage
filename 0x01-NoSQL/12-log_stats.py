999d20f731c3cefc1a0f0ccfe13bc3aa4ae01115
psych0wh1z <ikeagudike@gmail.com>
2023-08-17 13:10:31 +0100

    Add task12 script

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""12-log_stats"""
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nlogs = client.logs.nginx
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print("{} logs\nMethods:".format(nlogs.count_documents({})))
+
+    for method in methods:
+        print("\tmethod {}: {}".
+              format(method, nlogs.count_documents({'method': method})))
+
+    print("{} status check".
+          format(nlogs.count_documents({'method': 'GET', 'path': '/status'})))
