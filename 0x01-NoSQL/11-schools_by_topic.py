486bfd0b16a7fecde81f400c891afbb9e81486a3
psych0wh1z <ikeagudike@gmail.com>
2023-08-17 12:34:03 +0100

    Write more pythonic script

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -5,10 +5,7 @@
 def schools_by_topic(mongo_collection, topic):
     """Returns list of schools having a specific topic
     """
-    schools = []
-
-    for school in mongo_collection.find():
-        if topic in school.get('topics'):
-            schools.append(school)
+    schools = [school for school in mongo_collection.find()
+               if topic in school.get('topics')]
 
     return schools
