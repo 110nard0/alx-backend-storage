4dc47731ff5ac8ebe354df563ebbb9665adaeb06
psych0wh1z <ikeagudike@gmail.com>
2023-08-17 06:22:41 +0100

    Add task11 files

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""11-schools_by_topic.py"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Returns list of schools having a specific topic
+    """
+    schools = []
+
+    for school in mongo_collection.find():
+        if topic in school.get('topics'):
+            schools.append(school)
+
+    return schools
