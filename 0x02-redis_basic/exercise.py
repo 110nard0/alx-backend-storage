4f8cbd267c230d13700093d31384355f82ccb303
psych0wh1z <ikeagudike@gmail.com>
2023-08-19 10:40:16 +0100

    Add new methods and tests

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Module exercise"""
 
 import redis
-from typing import Union
+from typing import Callable, Optional, Union
 from uuid import uuid4
 
 
@@ -25,3 +25,43 @@ class Cache:
         rand_key = str(uuid4())
         self._redis.set(rand_key, data)
         return rand_key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
+            str, bytes, int, float]:
+        """Returns value associated with Redis key or None if nil
+
+        Args:
+            key (str): key to stored value in Redis cache
+            fn (Callable): Optional function to convert data to desired format
+
+        Returns:
+            value (any): value stored in Redis cache of default str type
+        """
+        value = self._redis.get(key)
+        if value and fn is not None:
+            value = fn(value)
+        return value
+
+    def get_int(self, key: str) -> int:
+        """Automatically parametrizes Cache.get method with the
+        appropriate conversion function
+
+        Argument:
+            key (str): key to stored value in Redis cache
+
+        Returns:
+            (int): integer value
+        """
+        return self.get(key, int)
+
+    def get_str(self, key: str) -> str:
+        """Automatically parametrizes Cache.get method with the
+        appropriate conversion function
+
+        Argument:
+            key (str): key to stored value in Redis cache
+
+        Returns:
+            (str): string value
+        """
+        return self.get(key, lambda d: d.decode('utf-8'))
