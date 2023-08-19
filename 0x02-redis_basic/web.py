0c20a0b9c8d36f8373306d6fbe2a8fe25beb3c8b
psych0wh1z <ikeagudike@gmail.com>
2023-08-19 22:30:58 +0100

    Add task5 file

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Module web"""
+
+import redis
+import requests
+from functools import wraps
+
+r = redis.Redis()
+
+
+def count_url(method):
+    """Decorator function that tracks the frequency with which
+        a particular URL is accessed
+    """
+    @wraps(method)
+    def wrapper(url):
+        r.incr(f"count:{url}")
+        return method(url)
+    return wrapper
+
+
+def cache_page(method):
+    """Decorator function that stores a web page in a Redis cache with
+        a 10 seconds expiration or returns a cached version if present
+    """
+    @wraps(method)
+    def wrapper(url):
+        cached_page = r.get(url)
+        if cached_page:
+            return cached_page.decode('utf-8')
+        else:
+            page = method(url)
+            r.setex(url, 10, page)
+            return page
+    return wrapper
+
+
+@count_url
+@cache_page
+def get_page(url: str) -> str:
+    """Gets the HTML content of a particular URL using requests module
+    """
+    page = requests.get(url).text
+    return page
