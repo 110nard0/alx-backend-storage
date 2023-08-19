8be598e93475075dc19b7b2902013750dcadc363
psych0wh1z <ikeagudike@gmail.com>
2023-08-19 23:01:07 +0100

    Fix main code

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,20 +1,50 @@
 #!/usr/bin/env python3
-""" Expiring web cache module """
+"""Module web contains functions to track url page caching"""
 
 import redis
 import requests
+from functools import wraps
+from typing import Callable
 
-redis = redis.Redis()
+r = redis.Redis()
 
-def get_page(url: str) -> str:
-    """get page HTML content
+
+def count_url(method: Callable):
+    """Decorator function that tracks the frequency with which
+        a particular URL is accessed
     """
-    redis.incr(f"count:{url}")
+    @wraps(method)
+    def wrapper(url):
+        """wrapper function"""
+        r.incr(method.__qualname__)
+        r.incr(f"count:{url}")
+        return method(url)
+    return wrapper
 
-    cached_response = redis.get(f"cached:{url}")
-    if cached_response:
-        return cached_response.decode('utf-8')
 
-    response = requests.get(url)
-    redis.setex(f"cached:{url}", 10, response)
-    return response.text
+def cache_page(method):
+    """Decorator function that stores a web page in a Redis cache with
+        a 10 seconds expiration or returns a cached version if present
+    """
+    @wraps(method)
+    def wrapper(url):
+        """wrapper function"""
+        key = f"{cached}:url"
+        cached_page = r.get(key)
+
+        if cached_page:
+            return cached_page.decode('utf-8')
+        else:
+            page = method(url)
+            r.setex(key, 10, page)
+            return page
+    return wrapper
+
+
+@count_url
+@cache_page
+def get_page(url: str) -> str:
+    """Gets the HTML content of a particular URL using requests module
+    """
+    page = requests.get(url).text
+    return page
