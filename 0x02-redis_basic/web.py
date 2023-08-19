f6fb4acd3d4feeeaa1702dcb7d8e8db3eabf2516
psych0wh1z <ikeagudike@gmail.com>
2023-08-19 22:51:02 +0100

    Test alternate code

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,48 +1,34 @@
 #!/usr/bin/env python3
-"""Module web"""
+""" expiring web cache module """
 
 import redis
 import requests
-from datetime import timedelta
+from typing import Callable
 from functools import wraps
 
-r = redis.Redis()
+redis = redis.Redis()
 
 
-def count_url(method):
-    """Decorator function that tracks the frequency with which
-        a particular URL is accessed
-    """
-    @wraps(method)
+def wrap_requests(fn: Callable) -> Callable:
+    """ Decorator wrapper """
+
+    @wraps(fn)
     def wrapper(url):
-        """wrapper function"""
-        r.incr(method.__qualname__)
-        r.incr(f"count:{url}")
-        return method(url)
+        """ Wrapper for decorator guy """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
+
     return wrapper
 
 
-def cache_page(method):
-    """Decorator function that stores a web page in a Redis cache with
-        a 10 seconds expiration or returns a cached version if present
-    """
-    @wraps(method)
-    def wrapper(url):
-        """wrapper function"""
-        cached_page = r.get(url)
-        if cached_page:
-            return cached_page.decode('utf-8')
-        else:
-            page = method(url)
-            r.setex(url, timedelta(seconds=10), value=page)
-            return page
-    return wrapper
-
-
-@count_url
-@cache_page
+@wrap_requests
 def get_page(url: str) -> str:
-    """Gets the HTML content of a particular URL using requests module
+    """get page self descriptive
     """
-    page = requests.get(url).text
-    return page
+    response = requests.get(url)
+    return response.text
